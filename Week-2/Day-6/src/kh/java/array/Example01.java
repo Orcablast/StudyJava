2721a424fa7c6665bf3a393f4f1fc07d2cddd2a0
Orcablast <59005472+Orcablast@users.noreply.github.com>
2020-02-04 09:47:45 +0900

    0204 수업시작 Day-6 프로젝트 생성
    수업내용 : array
    할당 패키지, 클래스 생성

---
+++ Week-2/Day-6/src/kh/java/array/Example01.java
@@ -0,0 +1,7 @@
+package kh.java.array;
+
+public class Example01 {
+	public void exam01() {
+		System.out.println("test");
+	}
+}
---
+++ Week-2/Day-6/src/kh/java/run/Start.java
@@ -0,0 +1,12 @@
+package kh.java.run;
+import kh.java.array.Example01;
+
+public class Start {
+
+	public static void main(String[] args) {
+		Example01 e01 = new Example01();
+		e01.exam01();
+
+	}
+
+}
