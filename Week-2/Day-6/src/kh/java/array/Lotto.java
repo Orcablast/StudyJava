d2edf7d3c3d14a9344f829a1da2bf8f39730cd75
Orcablast <59005472+Orcablast@users.noreply.github.com>
2020-02-05 10:35:38 +0900

    0205 로또 플러스 프로토타입

--- Week-2/Day-6/src/kh/java/array/Lotto.java
+++ Week-2/Day-6/src/kh/java/array/Lotto.java
@@ -47,11 +47,11 @@ public class Lotto {
 				} else {
 					userNum[i] = inputNum;
 					comNum[i] = rn.nextInt(45) + 1;
-					for(int j=i; j>0; j--) {
-						if(comNum[j-1]==comNum[i]) {
+					for (int j = i; j > 0; j--) {
+						if (comNum[j - 1] == comNum[i]) {
 							System.out.println("컴퓨터 같은값");
 							comNum[i] = rn.nextInt(45) + 1;
-							j=1;
+							j = i;
 						}
 					}
 				}
@@ -121,6 +121,118 @@ public class Lotto {
 	}
 
 	public void exam02() {
+		Scanner sc = new Scanner(System.in);
+		Random rn = new Random();
 
+		int userNum[] = new int[6];
+		int comNum[] = new int[6];
+		int countToWin = 1; // 1등까지 반복한 횟수 
+		
+		boolean win = false;
+
+		System.out.println("================= 로또 프로그램 =================");
+
+		for (int i = 0; i < userNum.length; i++) {
+
+			System.out.print((i + 1) + "번째 번호 입력 : ");
+			int userInput = sc.nextInt();
+
+			if (userInput > 46 || userInput <= 0) { // 입력단계에서 잘못된 숫자를 입력했을 경우
+
+				System.out.println("잘못 입력하셨습니다 ");
+				i--;
+				continue;
+			}
+
+			for (int j = i; j > 0; j--) {
+
+				if (userNum[j - 1] == userInput) {
+					System.out.println("중복된 번호입니다");
+
+					System.out.print((i + 1) + "번째 번호 입력 : ");
+					userInput = sc.nextInt();
+					j++;
+
+				}
+			}
+
+			userNum[i] = userInput;
+
+		}
+
+		while (!win) {
+			int countNum = 0;
+			
+			int temp;
+			
+			for (int i = 0; i < comNum.length; i++) {
+
+				for (int j = i; j > 0; j--) {
+					
+					comNum[i] = rn.nextInt(45) + 1;					
+
+					if (comNum[i] == comNum[j - 1]) {
+						j=i;
+					}
+				}
+
+				for (int j = 0; j < comNum.length; j++) {
+					
+					if (userNum[i] == comNum[j]) {
+						countNum++;
+					} else if (userNum[i] < userNum[j]) {
+						temp = userNum[j];
+						userNum[j] = userNum[i];
+						userNum[i] = temp;
+					}else if (comNum[i] < comNum[j]) {
+						temp = comNum[j];
+						comNum[j] = comNum[i];
+						comNum[i] = temp;
+								
+					}
+				}				
+			}
+			
+			System.out.printf("============ %d회차 ============",countToWin);
+
+			System.out.println("사용자 번호");
+			for(int i=0; i<userNum.length; i++) {
+				System.out.print(userNum[i]+"  ");
+			}
+			System.out.println();
+			
+			System.out.println("컴퓨터 번호");
+			for(int i=0; i<userNum.length; i++) {
+				System.out.print(comNum[i]+"  ");
+			}
+			System.out.println();
+			
+			System.out.print("맞은 갯수 : " + countNum);
+			
+			switch (countNum) {
+			case 0:
+			case 1:
+			case 2:
+				System.out.println("꽝!!!");
+				break;
+			case 3:
+				System.out.println("5등!");
+				break;
+			case 4:
+				System.out.println("4등!!");
+				break;
+			case 5:
+				System.out.println("3등!!");
+				break;
+			case 6:
+				System.out.println("1등!! 축하합니다!!!");
+				win = true;
+			}
+			win = true;
+		} // 1등일시 while 종료
+	}
+	
+	public void exam03() {
+		
 	}
 }
