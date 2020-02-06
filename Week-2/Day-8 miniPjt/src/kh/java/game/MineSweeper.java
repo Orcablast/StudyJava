5427a9339f83a41a5a113365ec088201f007a141
Orcablast <59005472+Orcablast@users.noreply.github.com>
2020-02-06 21:18:10 +0900

    0206 지뢰찾기 업데이트
    1. 유저 넘버 체크 완료

--- Week-2/Day-8 miniPjt/src/kh/java/game/MineSweeper.java
+++ Week-2/Day-8 miniPjt/src/kh/java/game/MineSweeper.java
@@ -2,8 +2,6 @@ package kh.java.game;
 
 import java.util.Scanner;
 
-import org.omg.Messaging.SyncScopeHelper;
-
 import java.util.Random;
 
 public class MineSweeper {
@@ -12,6 +10,8 @@ public class MineSweeper {
 
 	int gameNum;
 	int mineNum;
+
+	boolean boom = false; // 유저가 지뢰를 밟았는지 논리 여부
 
 	public void main() { // 메인 메뉴
 		while (true) {
@@ -36,16 +36,16 @@ public class MineSweeper {
 				int back[][] = new int[gameNum][gameNum]; // 지뢰가 있는 배열 선언
 				int front[][] = new int[gameNum][gameNum]; // 유저에게 보여줄 배열
 
-				boolean boom= false; // 유저가 지뢰를 밟았는지 논리 여부 
-
 				putMine(back);
-				
-				while(!boom) { // 유저가 지뢰를 밟을때까지 반복
 
-					showCurrent(back); // 화면 출력 메소드
+				while (!boom) {
+
+					showCurrent(back); // 화면 출력 메소드, 테스트용
+
+					showScreen(back);
+
+					userSelect(back);
 				}
-				
-				showScreen(back);
 
 				continue;
 
@@ -89,18 +89,18 @@ public class MineSweeper {
 
 	public void showScreen(int arr[][]) { // 사용자 화면
 		int k = 1;
-		
+
 		int mineCount;
-		
+
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr.length; j++) {
-				
-				mineCount=0;
-				
-				switch(arr[i][j]) {
+
+				mineCount = 0;
+
+				switch (arr[i][j]) {
 				case -1:
 				case 0:
-					if(k<10) {
+					if (k < 10) {
 						System.out.print("[");
 						System.out.print("0" + k++);
 						System.out.print("]");
@@ -110,108 +110,166 @@ public class MineSweeper {
 						System.out.print("]");
 					}
 					break;
-					
+
 				case 2:
 					mineCount = check(arr, i, j);
-					
-					if(mineCount==0) {
+
+					if (mineCount == 0) {
 						System.out.print("    ");
 					} else {
-						System.out.print("  " + mineCount + " " );
+						System.out.print("  " + mineCount + " ");
 					}
 					k++;
-					break;					
+					break;
 				}
-				
-//				if (arr[i][j] < 1 && k < 10) { // 초기상태 중 k가 10 미만 일 때
-//					System.out.print("[");
-//					System.out.print("0" + k++);
-//					System.out.print("]");
-//
-//				} else if (arr[i][j] < 1) { // 초기상태
-//					System.out.print("[");
-//					System.out.print(k++);
-//					System.out.print("]");
-//				} else if (arr[i][j] == 2) { // 공백상태 (배열값 2)
-//					k++;
-//					System.out.print("  " + check(arr, i, j) + " " );
-//				} else if (arr[i][j] == 0) {
-//					k++;
-//					System.out.print("[  ]");
-//
-//				}
+
+				// if (arr[i][j] < 1 && k < 10) { // 초기상태 중 k가 10 미만 일 때
+				// System.out.print("[");
+				// System.out.print("0" + k++);
+				// System.out.print("]");
+				//
+				// } else if (arr[i][j] < 1) { // 초기상태
+				// System.out.print("[");
+				// System.out.print(k++);
+				// System.out.print("]");
+				// } else if (arr[i][j] == 2) { // 공백상태 (배열값 2)
+				// k++;
+				// System.out.print(" " + check(arr, i, j) + " " );
+				// } else if (arr[i][j] == 0) {
+				// k++;
+				// System.out.print("[ ]");
+				//
+				// }
 			}
 			System.out.println();
-		}
+		} // 2중 for문 종료시점
 	}
-	
-	
-	public void userSelect() {
-		int userNum=(gameNum*gameNum);
-		
-		while(true) {
-			System.out.println("숫자를 선택하세요! (1~"+(gameNum*gameNum)+")");
+
+	public void userSelect(int arr[][]) {
+		int userNum;
+
+		while (true) {
+			System.out.println("숫자를 선택하세요! (1~" + (gameNum * gameNum) + ")");
 			userNum = sc.nextInt();
-			
-			if(userNum<0 || userNum > (gameNum*gameNum)) {
+
+			if (userNum < 0 || userNum > (gameNum * gameNum)) {
 				System.out.println("잘못된 입력입니다!");
-			} else {
-				
-				
-			}
-			
-		}
-		
-	}
-	
-	public int check(int arr[][], int row, int column) {
-		int mineCount = 0;
-		
-		
-		for(int r=row-1; r<=row+1; r++ ) {
-			
-			if(r<0 || r>gameNum-1) {
-				
 				continue;
-				
 			} else {
+
+				int row = 0;
+				int column = 0;
+
+				for (int i=0; (i<gameNum) && userNum!=0; i++) {
+					row = i;
+					for (int j=0; (j<gameNum) && userNum!=0; j++) {
+						column = j;
+						userNum--;
+					}
+				}
 				
-				for(int c=column-1; c<=column+1; c++) {
-					
-					if(c<0 || c>gameNum-1) {
-						
-						continue;
-					}					
-					
-					if(arr[r][c]==-1) {
-						mineCount++;
-					}					
+				if (arr[row][column] == -1) {
+
+					System.out.println("게임 오버!!");
+
+					boom = true;
+					return;
+
+				} else if (arr[row][column] == 0) {
+					arr[row][column] = 2;
+					return;
+
+				} else {
+					System.out.println("잘못 입력하셨습니다");
 				}
 			}
 		}
-		
-		
-		
+	}
+
+	public void userNumChk(int num, int arr[][]) {
+		int row = 0;
+		int column = 0;
+
+		for (int i = 0; i < gameNum - 1; i++) {
+
+			if (num == 0) {
+				break;
+			} else {
+				row++;
+				num--;
+			}
+
+			for (int j = 0; j < gameNum - 1; j++) {
+
+				if (num == 0) {
+					break;
+				} else {
+					column++;
+					num--;
+				}
+			}
+		}
+
+		if (arr[row][column] == -1) {
+			boom = true;
+			return;
+		} else if (arr[row][column] == 0) {
+			arr[row][column] = 2;
+			return;
+		} else {
+			System.out.println("잘못 입력하셨습니다");
+		}
+	}
+
+	public int check(int arr[][], int row, int column) {
+		int mineCount = 0;
+
+		for (int r = row - 1; r <= row + 1; r++) {
+
+			if (r < 0 || r > gameNum - 1) {
+
+				continue;
+
+			} else {
+
+				for (int c = column - 1; c <= column + 1; c++) {
+
+					if (c < 0 || c > gameNum - 1) {
+
+						continue;
+					}
+
+					if (arr[r][c] == -1) {
+						mineCount++;
+					}
+				}
+			}
+		}
+
 		return mineCount;
-		
-// 		// 최초에 구상한 Boundary 로직
-//		if (row == 0 && column == 0) { // row 0, column 0 -> 좌상
-//		
-//		} else if (row == 0 && column == gameNum - 1) { // row 0, column 4 -> 우상
-//
-//		} else if (row == gameNum - 1 && column == 0) { // row 4, column 0 -> 좌하
-//
-//		} else if (row == gameNum - 1 && column == gameNum - 1) { // row 4, column 4 -> 우하
-//
-//		} else if (row == 0 && (column != 0 || column != gameNum - 1)) { // row 0, 첫번째행
-//
-//		} else if (row == gameNum - 1 && (column != gameNum - 1 || column != 0)) { // row 4, 마지막행
-//
-//		} else if (column == 0 && (row != 0 || row != gameNum - 1)) { // column 0, 좌측 첫째열
-//
-//		} else if (column == gameNum - 1 && (row != 0 || row != gameNum - 1)) {
-//
-//		}
+
+		// // 최초에 구상한 Boundary 로직
+		// if (row == 0 && column == 0) { // row 0, column 0 -> 좌상
+		//
+		// } else if (row == 0 && column == gameNum - 1) { // row 0, column 4 -> 우상
+		//
+		// } else if (row == gameNum - 1 && column == 0) { // row 4, column 0 -> 좌하
+		//
+		// } else if (row == gameNum - 1 && column == gameNum - 1) { // row 4, column 4
+		// -> 우하
+		//
+		// } else if (row == 0 && (column != 0 || column != gameNum - 1)) { // row 0,
+		// 첫번째행
+		//
+		// } else if (row == gameNum - 1 && (column != gameNum - 1 || column != 0)) { //
+		// row 4, 마지막행
+		//
+		// } else if (column == 0 && (row != 0 || row != gameNum - 1)) { // column 0, 좌측
+		// 첫째열
+		//
+		// } else if (column == gameNum - 1 && (row != 0 || row != gameNum - 1)) {
+		//
+		// }
 	}
 
 	public void delay(int time) {
