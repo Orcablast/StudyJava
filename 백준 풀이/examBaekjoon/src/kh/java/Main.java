28af2d8d3781a17a1922d5a65e36c2278c51a6eb
Orcablast <59005472+Orcablast@users.noreply.github.com>
2020-03-27 19:43:28 +0900

    0327 daily commit

--- 백준 풀이/examBaekjoon/src/kh/java/Main.java
+++ 백준 풀이/examBaekjoon/src/kh/java/Main.java
@@ -1,30 +1,33 @@
 package kh.java;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		try {
-			int a = Integer.parseInt(br.readLine());
-			int b = Integer.parseInt(br.readLine());
 			
-			System.out.println();
+			String str = br.readLine();
+			StringTokenizer sT = new StringTokenizer(str, " ");
 			
+			int h = Integer.parseInt(sT.nextToken());
+			int m = Integer.parseInt(sT.nextToken());			
+						
+			if (m<45){
+				h = (h==0 ? 23 : h);
+				m += (60-45);
+			} else {
+				m -= 45;
+			}
+			
+			System.out.println(h+" "+m);
+
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				br.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}		
+		}
 	}
 }
